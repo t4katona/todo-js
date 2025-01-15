04871c9a4964081ae57be5ef1465b67a1e194702
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2025-01-15 15:52:11 +0100

    added transition for category change modal on save

--- modules/listeners/tasks/save-category-change.listeners.js
+++ modules/listeners/tasks/save-category-change.listeners.js
@@ -3,8 +3,8 @@ import {
   buttons,
   options,
 } from "../../constants/ui-dom-elements.constants.js";
-
 import { taskManager } from "../../task-manager.js";
+import { modalClosingTransition } from "../../constants/modal-close-transition.constant.js";
 
 (() => {
   buttons.saveCategoryChange.addEventListener("click", (e) => {
@@ -12,6 +12,8 @@ import { taskManager } from "../../task-manager.js";
     const selectedCategory = options[dropdown.selectedIndex].value;
     const task = taskManager.findTask(modals.modalCategoryChange.dataset.id);
     taskManager.updateTaskCategory(task, selectedCategory);
+    modals.modalOverlay.style.transition = modalClosingTransition;
+
     modals.modalOverlay.classList.remove("show-modal");
   });
 })();
