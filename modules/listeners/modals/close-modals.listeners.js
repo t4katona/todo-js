4da6fac99b980d3e8408945319779ce44679b695
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2025-01-15 15:30:44 +0100

    added transitions for modal opens and closes

--- modules/listeners/modals/close-modals.listeners.js
+++ modules/listeners/modals/close-modals.listeners.js
@@ -1,13 +1,12 @@
-import {
-    buttons,
-    modals,
-  } from "../../constants/ui-dom-elements.constants.js"; // <-- import different 
+import { buttons, modals } from "../../constants/ui-dom-elements.constants.js";
+import { modalClosingTransition } from "../../constants/modal-close-transition.constant.js";
 
 (() => {
-    buttons.modalClosingBtns.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-          e.preventDefault();
-          modals.modalOverlay.classList.remove("show-modal");
-        });
-      });
+  buttons.modalClosingBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      modals.modalOverlay.style.transition = modalClosingTransition;
+      modals.modalOverlay.classList.remove("show-modal");
+    });
+  });
 })();
--- modules/listeners/tasks/create-task.listeners.js
+++ modules/listeners/tasks/create-task.listeners.js
@@ -1,11 +1,13 @@
 import { buttons, modals } from "../../constants/ui-dom-elements.constants.js";
 import { getCreateTaskModalFormInputs } from "../../modal-helpers/create-task-modal.helpers.js";
 import { taskManager } from "../../task-manager.js";
+import { modalClosingTransition } from "../../constants/modal-close-transition.constant.js";
 
 (() => {
   buttons.createTaskBtn.addEventListener("click", (e) => {
     e.preventDefault();
     taskManager.addTask(getCreateTaskModalFormInputs());
+    modals.modalOverlay.style.transition = modalClosingTransition;
     modals.modalOverlay.classList.remove("show-modal");
   });
 })();
