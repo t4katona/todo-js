51b2fa567be145164fbee0ad5e5fcbf2e65b836f
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2025-01-15 15:17:30 +0100

    added hover to primary buttons, modals are now closed by default after an action

--- modules/listeners/tasks/create-task.listeners.js
+++ modules/listeners/tasks/create-task.listeners.js
@@ -1,4 +1,4 @@
-import { buttons } from "../../constants/ui-dom-elements.constants.js";
+import { buttons, modals } from "../../constants/ui-dom-elements.constants.js";
 import { getCreateTaskModalFormInputs } from "../../modal-helpers/create-task-modal.helpers.js";
 import { taskManager } from "../../task-manager.js";
 
@@ -6,5 +6,6 @@ import { taskManager } from "../../task-manager.js";
   buttons.createTaskBtn.addEventListener("click", (e) => {
     e.preventDefault();
     taskManager.addTask(getCreateTaskModalFormInputs());
+    modals.modalOverlay.classList.remove("show-modal");
   });
 })();
--- modules/listeners/tasks/save-category-change.listeners.js
+++ modules/listeners/tasks/save-category-change.listeners.js
@@ -12,5 +12,6 @@ import { taskManager } from "../../task-manager.js";
     const selectedCategory = options[dropdown.selectedIndex].value;
     const task = taskManager.findTask(modals.modalCategoryChange.dataset.id);
     taskManager.updateTaskCategory(task, selectedCategory);
+    modals.modalOverlay.classList.remove("show-modal");
   });
 })();
